f882880a5e4c64d594d30c7421b3a5dcd5478d51
github username <andrewcohn0@gmail.com>
2017-11-01 15:16:53 -0400

    scafooled some actions && reducers

---
+++ auth/auth-frontend/src/actions/user.js
@@ -0,0 +1,3 @@
+export function loginUser(username, password) {
+  //TODO: how can we log the user in??????
+}
---
+++ auth/auth-frontend/src/reducers/usersReducer.js
@@ -0,0 +1,8 @@
+export default function usersReducer(state = { username: null }, action) {
+  //TODO: fill this bad boy out
+  switch (action.type) {
+    case expression:
+    default:
+      return state
+  }
+}
