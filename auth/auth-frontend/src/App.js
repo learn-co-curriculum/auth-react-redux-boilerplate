6b50d52bbe138ba8985cb283781755ea86ced7e4
github username <andrewcohn0@gmail.com>
2017-11-01 13:20:10 -0400

    basic app component; still need LoginForm

--- auth/auth-frontend/src/App.js
+++ auth/auth-frontend/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Route, NavLink } from 'react-router-dom'
+import Profile from './components/Profile'
+import Login from './components/Login'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/profile">Profile</NavLink>
+      {/* <Route exact path="/profile" component={TODO: create an HOC that renders out the profile if user is logged in} /> */}
+      <Route exact path="/login" componnet={LoginForm} />
+    </div>
+  )
 }
 
 export default App;
